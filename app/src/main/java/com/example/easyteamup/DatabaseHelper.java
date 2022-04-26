c400a88c86115f9cb6b3e8f919abfc0dcbb3a127
Xinran Gao <sherrygao821@hotmail.com>
2022-04-25 22:38:39 -0700

    finished map view -> event detail

--- app/src/main/java/com/example/easyteamup/DatabaseHelper.java
+++ app/src/main/java/com/example/easyteamup/DatabaseHelper.java
@@ -118,15 +118,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public boolean checkUser(String username) {
         SQLiteDatabase db = this.getWritableDatabase();
 
-        db.execSQL("DROP TABLE IF EXISTS " + EVENT_TABLE);
-        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE);
-        db.execSQL("DROP TABLE IF EXISTS " + NOTIFICATION_TABLE);
-        String createUserTableStatement = "CREATE TABLE " + USER_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME + " TEXT, " + COLUMN_AGE + " INT, " + COLUMN_EMAIL + " TEXT, " + COLUMN_PWD + " TEXT, " + COLUMN_STUDENT + " INT, " + COLUMN_NOTICNT + " INT, " + COLUMN_INVCNT + " INT)";
-        String createEventTableStatement = "CREATE TABLE " + EVENT_TABLE + " (" + COLUMN_EVT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_EVT_NAME + " TEXT, " + COLUMN_HOST_ID + " INT, " + COLUMN_TIME + " TEXT, "  + COLUMN_LOCATION + " TEXT, " + COLUMN_TIMESLOTS + " TEXT, " + COLUMN_PARTICIPANTS + " TEXT, " + COLUMN_EVT_DURATION + " TEXT, " + COLUMN_EVT_TYPE + " INT, " + COLUMN_EVT_DESCRIPTION + " TEXT, " + COLUMN_EVT_PUBLIC + " BOOLEAN, " + COLUMN_EVT_DETERMINED_TIME + " TEXT )";
-        String createNotiTableStatement = "CREATE TABLE " + NOTIFICATION_TABLE + " (" + COLUMN_NOTIFICATION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_EVT_ID + " INT, " + COLUMN_FROM_ID + " INT, " + COLUMN_TO_ID + " INT, " + COLUMN_NOTIFICATION_TYPE + " INT)";
-        db.execSQL(createEventTableStatement);
-        db.execSQL(createUserTableStatement);
-        db.execSQL(createNotiTableStatement);
+//        db.execSQL("DROP TABLE IF EXISTS " + EVENT_TABLE);
+//        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE);
+//        db.execSQL("DROP TABLE IF EXISTS " + NOTIFICATION_TABLE);
+//        String createUserTableStatement = "CREATE TABLE " + USER_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME + " TEXT, " + COLUMN_AGE + " INT, " + COLUMN_EMAIL + " TEXT, " + COLUMN_PWD + " TEXT, " + COLUMN_STUDENT + " INT, " + COLUMN_NOTICNT + " INT, " + COLUMN_INVCNT + " INT)";
+//        String createEventTableStatement = "CREATE TABLE " + EVENT_TABLE + " (" + COLUMN_EVT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_EVT_NAME + " TEXT, " + COLUMN_HOST_ID + " INT, " + COLUMN_TIME + " TEXT, "  + COLUMN_LOCATION + " TEXT, " + COLUMN_TIMESLOTS + " TEXT, " + COLUMN_PARTICIPANTS + " TEXT, " + COLUMN_EVT_DURATION + " TEXT, " + COLUMN_EVT_TYPE + " INT, " + COLUMN_EVT_DESCRIPTION + " TEXT, " + COLUMN_EVT_PUBLIC + " BOOLEAN, " + COLUMN_EVT_DETERMINED_TIME + " TEXT )";
+//        String createNotiTableStatement = "CREATE TABLE " + NOTIFICATION_TABLE + " (" + COLUMN_NOTIFICATION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_EVT_ID + " INT, " + COLUMN_FROM_ID + " INT, " + COLUMN_TO_ID + " INT, " + COLUMN_NOTIFICATION_TYPE + " INT)";
+//        db.execSQL(createEventTableStatement);
+//        db.execSQL(createUserTableStatement);
+//        db.execSQL(createNotiTableStatement);
 
         Cursor cursor = db.rawQuery("SELECT ID FROM USER_TABLE WHERE EMAIL = ?", new String[] {username});
         if(cursor.getCount() > 0)
--- app/src/main/java/com/example/easyteamup/ui/MapsFragment.java
+++ app/src/main/java/com/example/easyteamup/ui/MapsFragment.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.easyteamup.DatabaseHelper;
+import com.example.easyteamup.EventDetail;
 import com.example.easyteamup.R;
 import com.example.easyteamup.classes.Event;
 import com.example.easyteamup.classes.Location;
@@ -21,16 +23,20 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapsFragment extends Fragment {
 
     Geocoder gc;
     DatabaseHelper db;
+    GoogleMap gm;
 
     List<Event> events;
 
@@ -47,6 +53,7 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
+            gm = googleMap;
             db = new DatabaseHelper(getActivity());
             events = db.getAllActivePublicEvents();
 
@@ -56,11 +63,26 @@ public class MapsFragment extends Fragment {
                 Location loc = new Gson().fromJson(loc_string, Location.class);
 
                 LatLng loc_latLong = new LatLng(loc.getLatitude(), loc.getLongitude());
-
-                googleMap.addMarker(new MarkerOptions().position(loc_latLong).title(loc.getAddress()));
+                Marker marker = googleMap.addMarker(new MarkerOptions().position(loc_latLong).title(e.getEvtName()));
+                marker.setTag(e);
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc_latLong));
-
             }
+
+            gm.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                @Override
+                public void onInfoWindowClick(Marker marker) {
+
+                    Event e = (Event)marker.getTag();
+                    String hostEmail = db.getUserEmail(e.getHostId());
+                    e.setHostEmail(hostEmail);
+                    String e_string = new Gson().toJson(e);
+
+                    Intent intent = new Intent(getActivity(), EventDetail.class);
+                    intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    intent.putExtra("eventInfo", e_string);
+                    startActivity(intent);
+                }
+            });
         }
     };
 
@@ -82,4 +104,6 @@ public class MapsFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
     }
+
+
 }
